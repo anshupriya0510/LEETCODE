deab99a22177c35ef86b7f0a0a4197d5634c6b04
Anshu Priya <anshupriya05102004@gmail.com>
2026-03-25 23:50:18 +0530

    Time: 5 ms (47.55%), Space: 161.7 MB (41.96%) - LeetHub

---
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean canPartitionGrid(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        long total = 0;
+
+        // compute total sum
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                total += grid[i][j];
+            }
+        }
+
+        if (total % 2 != 0) return false;
+
+        long target = total / 2;
+
+        // check horizontal cut
+        long sum = 0;
+        for (int i = 0; i < m - 1; i++) {
+            for (int j = 0; j < n; j++) {
+                sum += grid[i][j];
+            }
+            if (sum == target) return true;
+        }
+
+        // compute column sums
+        long[] colSum = new long[n];
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < m; i++) {
+                colSum[j] += grid[i][j];
+            }
+        }
+
+        // check vertical cut
+        sum = 0;
+        for (int j = 0; j < n - 1; j++) {
+            sum += colSum[j];
+            if (sum == target) return true;
+        }
+
+        return false;
+    }
+}
